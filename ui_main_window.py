769d6d6690bdbb26abf0b0b1913a3b9a52173acd
goktu-o <92593521+goktu-o@users.noreply.github.com>
2024-02-29 14:14:52 +0300

    Add files via upload

--- ui_main_window.py
+++ ui_main_window.py
@@ -3,11 +3,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(800, 640)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 101, 640, 480))
+        self.label.setGeometry(QtCore.QRect(10, 150, 640, 480))
         self.label.setObjectName("label")        
         self.recordButton = QtWidgets.QPushButton(self.centralwidget)
         self.recordButton.setGeometry(QtCore.QRect(670, 30, 120, 40))
@@ -19,11 +19,11 @@ class Ui_MainWindow(object):
 
         # Add a QLabel for your logo
         self.logoLabel = QtWidgets.QLabel(self.centralwidget)
-        self.logoLabel.setGeometry(QtCore.QRect(10, 10, 99, 91))
+        self.logoLabel.setGeometry(QtCore.QRect(10, 10, 100, 135))
         self.logoLabel.setObjectName("logoLabel")
 
         # Set the pixmap of the logoLabel to your logo image
-        logo_path = "assets/tugrul-logo_99x91px.png"
+        logo_path = "assets/tugrul-logo4_100x135px.png"
         self.logoLabel.setPixmap(QtGui.QPixmap(logo_path))
 
         MainWindow.setCentralWidget(self.centralwidget)
